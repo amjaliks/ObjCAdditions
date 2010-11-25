fb2cb1af8f44657a4adde7d369823b1352515abf
Aleksejs Mjaliks <am@keeper.lv>
2010-11-25 20:32:10 +0200

    Added tests for Mac OS X additions

---
+++ Source/Tests/Additions/NSDictionaryTests.h
@@ -0,0 +1,16 @@
+//
+//  NSDictionaryTests.h
+//  ObjCAdditions-MacOSX
+//
+//  Created by Aleksejs Mjaliks on 10.11.25.
+//  Copyright 2010 A25. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface NSDictionaryTests : SenTestCase {
+
+}
+
+@end
