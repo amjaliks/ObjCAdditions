3fd79a36ac489f8e58d84661a2521da6de988457
Aleksejs Mjaliks <am@keeper.lv>
2010-12-18 00:50:01 +0200

    Added OAAudioPlayer

---
+++ Source/Classes/OAAudioPlayer.h
@@ -0,0 +1,16 @@
+//
+//  OAAudioPlayer.h
+//  ObjCAdditions-iOS
+//
+//  Created by Aleksejs Mjaliks on 10.12.18.
+//  Copyright 2010 A25. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface OAAudioPlayer : NSObject {
+
+}
+
+@end
---
+++ Source/Classes/OAAudioPlayer.m
@@ -0,0 +1,14 @@
+//
+//  OAAudioPlayer.m
+//  ObjCAdditions-iOS
+//
+//  Created by Aleksejs Mjaliks on 10.12.18.
+//  Copyright 2010 A25. All rights reserved.
+//
+
+#import "OAAudioPlayer.h"
+
+
+@implementation OAAudioPlayer
+
+@end
