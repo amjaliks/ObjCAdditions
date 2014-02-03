4d2ca4e00522b29595988b6479a66639d2fbc8a4
Aleksejs Mjaliks <am@keeper.lv>
2014-02-03 22:19:49 +0200

    Fixed arm64 related warnings

--- Source/Additions/NSString+MD5.m
+++ Source/Additions/NSString+MD5.m
@@ -14,7 +14,7 @@
 	
 	unsigned char result[CC_MD5_DIGEST_LENGTH];
 	
-	CC_MD5(cStr, strlen(cStr), result );
+	CC_MD5(cStr, (CC_LONG)strlen(cStr), result );
 	return [NSString stringWithFormat:
 				@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
 				result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
